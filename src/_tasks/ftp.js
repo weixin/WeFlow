eaacafe15305067e219d0b1db22e26292cdb2f34
littledu <littledu@tencent.com>
2017-06-15 11:09:00 +0800

    更新example

--- src/_tasks/ftp.js
+++ src/_tasks/ftp.js
@@ -6,70 +6,67 @@ const gulp = require('gulp');
 const _ = require('lodash');
 const del = require('del');
 const ftp = require('gulp-ftp');
+const gutil = require('gulp-util');
 const Common = require(path.join(__dirname, '../common'));
 
 module.exports = function (projectPath, log, callback) {
 
-    let projectConfigPath = path.join(projectPath, 'weflow.config.json');
-    let config = null;
+  let distDir = path.join(projectPath, './dist');
 
-    if (Common.fileExist(projectConfigPath)) {
-        config = Common.requireUncached(projectConfigPath);
-    } else {
-        config = Common.requireUncached(path.join(__dirname, '../../weflow.config.json'));
-    }
+  let projectConfigPath = path.join(projectPath, 'weflow.config.json');
+  let config = null;
 
-    let configFTP = config.ftp;
+  if (Common.fileExist(projectConfigPath)) {
+    config = Common.requireUncached(projectConfigPath);
+  } else {
+    config = Common.requireUncached(path.join(__dirname, '../../weflow.config.json'));
+  }
 
-    if (configFTP.host === '' || configFTP.pass === '' || configFTP.user === '') {
-        callback('ftp config');
-        return;
-    }
+  let configFTP = config.ftp;
 
-    let projectName = path.basename(projectPath);
+  if (configFTP.host === '' || configFTP.pass === '' || configFTP.user === '') {
+    callback('ftp config');
+    return;
+  }
 
-    //清除目标目录
-    function delDist(cb) {
-        del(path.join(projectPath, './dist'), {force: true}).then(function () {
-            cb && cb();
-        })
-    }
+  let projectName = path.basename(projectPath);
 
-    function remoteFtp(cb) {
-        let remotePath = config['ftp']['remotePath'] || "";
-        let ftpConfig = _.extend(config['ftp'], {
-            remotePath: path.join(remotePath, projectName)
-        });
-        let distPath = config['ftp']['includeHtml'] ? path.join(projectPath, './dist/**/*') : [path.join(projectPath, './dist/**/*'), path.join(projectPath, '!./dist/html/**/*.html')];
+  //清除目标目录
+  function delDist(cb) {
+    del(path.join(projectPath, './dist'), {force: true}).then(function () {
+      cb && cb();
+    })
+  }
 
-        console.log(ftpConfig)
-        console.log(distPath);
-
-        gulp.src(distPath)
-            .pipe(ftp(ftpConfig))
-            .on('finish', function(){
-                console.log('sftp success.');
-                log('sftp success.');
-                cb && cb();
-            })
-            .on('end', function () {
-                console.log('ftp success.');
-                log('ftp success.');
-                cb && cb();
-            });
-    }
-
-    async.series([
-        function (next) {
-            remoteFtp(next);
-        }
-    ], function (err) {
-        if (err) {
-            throw new Error(err);
-        }
-
-        delDist();
-
-        callback && callback();
+  function remoteFtp(cb) {
+    let remotePath = config['ftp']['remotePath'] || "";
+    let ftpConfig = _.extend(config['ftp'], {
+      remotePath: path.join(remotePath, projectName)
     });
+    let distPath = config['ftp']['includeHtml'] ? path.join(projectPath, './dist/**/*') : [path.join(projectPath, './dist/**/*'), path.join(projectPath, '!./dist/html/**/*.html')];
+
+    gulp.src(distPath)
+      .pipe(ftp(ftpConfig))
+      .pipe(gutil.noop())
+      .pipe(gulp.dest(distDir))
+      .on('end', function () {
+        console.log('ftp success.');
+        log('ftp success.');
+        cb && cb();
+      })
+  }
+
+  async.series([
+    function (next) {
+      remoteFtp(next);
+    }
+  ], function (err) {
+    if (err) {
+      throw new Error(err);
+    }
+
+    delDist();
+
+    callback && callback();
+  });
 };
